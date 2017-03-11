19bbeed139be4146513cce1b43519145128e7c0b
weimingtom <weimingtom@qq.com>
2017-03-12 00:11:59 +0800

    headers

---
+++ lua-5.3.4/kopilua/dummy.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace KopiLua
+{
+	public partial class Lua
+	{
+		public Lua()
+		{
+			
+		}
+		
+		public static void api_check(lua_State L, bool b, string str)
+		{
+			
+		}
+		
+		public static int luaK_codeABx(FuncState fs, OpCode o, int A, int sBx)
+		{
+			return 0;
+		}
+		
+		public static void luaK_setreturns(FuncState fs, expdesc e, int arg3)
+		{
+			
+		}
+		
+		public static int luaK_jump(FuncState fs)
+		{
+			return 0;
+		}
+		
+		public static void luaK_patchlist(FuncState fs, int arg2, int t)
+		{
+			
+		}
+		
+		public static int LUA_MULTRET = 0;
+		public static int MAXARG_sBx = 0;
+	}
+	
+	public class lua_State 
+	{
+		public int top;
+
+		public class ci_cls {
+			public int top;
+			public int func;
+		}
+		
+		public ci_cls ci;
+	}
+	
+	public class Test
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+	
+	public class InstructionPtr
+	{
+		public InstructionPtr(int arg1, int arg2){}
+	}
+	
+	public class FuncState
+	{
+		public class f_cls {public int code;};
+		public f_cls f;
+	}
+	
+	public class expdesc
+	{
+		public class u_cls {public int info;};
+		public u_cls u;
+	}
+	
+	public class OpCode
+	{
+		
+	}
+}
--- lua-5.3.4/kopilua/lapi.h.cs
+++ lua-5.3.4/kopilua/lapi.h.cs
@@ -3,22 +3,19 @@
 ** Auxiliary functions from Lua API
 ** See Copyright Notice in lua.h
 */
+namespace KopiLua
+{
 
-#ifndef lapi_h
-#define lapi_h
+	public partial class Lua
+	{
 
+		public static void api_incr_top(lua_State L)   {L.top++; api_check(L, L.top <= L.ci.top, "stack overflow");}
 
-#include "llimits.h"
-#include "lstate.h"
+		public static void adjustresults(lua_State L, int nres)
+    	{ if ((nres) == LUA_MULTRET && L.ci.top < L.top) L.ci.top = L.top; }
 
-#define api_incr_top(L)   {L->top++; api_check(L, L->top <= L->ci->top, \
-				"stack overflow");}
+		public static void api_checknelems(lua_State L, int n)	{ api_check(L, (n) < (L.top - L.ci.func),
+			"not enough elements in the stack"); }
 
-#define adjustresults(L,nres) \
-    { if ((nres) == LUA_MULTRET && L->ci->top < L->top) L->ci->top = L->top; }
-
-#define api_checknelems(L,n)	api_check(L, (n) < (L->top - L->ci->func), \
-				  "not enough elements in the stack")
-
-
-#endif
+	}
+}
