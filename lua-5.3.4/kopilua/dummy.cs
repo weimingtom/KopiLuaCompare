36f5cf873589c2f4a911ef51ef9c17addbede88b
weimingtom <weimingtom@qq.com>
2017-05-07 15:44:14 +0800

    lzio.h

--- lua-5.3.4/kopilua/dummy.cs
+++ lua-5.3.4/kopilua/dummy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace KopiLua
 {
@@ -137,6 +138,13 @@ namespace KopiLua
 				return 0;
 			}
 		}
+		
+		public static int cast_uchar(char c) { return (int)c; } //FIXME: return char???
+		
+		public static int luaZ_fill(ZIO z) 
+		{
+			return 0;
+		}
 	}
 	
 	public class lua_State 
@@ -161,7 +169,8 @@ namespace KopiLua
 	{
 		static void Main(string[] args)
 		{
-			
+			Console.WriteLine("hello, world!");
+			Console.ReadLine();
 		}
 	}
 	
@@ -256,7 +265,131 @@ namespace KopiLua
 	
 	public class CharPtr
 	{
+		public char[] chars;
+		public int index;
 		
+		public char this[int offset]
+		{
+			get { return chars[index + offset]; }
+			set { chars[index + offset] = value; }
+		}
+		public char this[uint offset]
+		{
+			get { return chars[index + offset]; }
+			set { chars[index + offset] = value; }
+		}
+		public char this[long offset]
+		{
+			get { return chars[index + (int)offset]; }
+			set { chars[index + (int)offset] = value; }
+		}
+
+		public static implicit operator CharPtr(string str) { return new CharPtr(str); }
+		public static implicit operator CharPtr(char[] chars) { return new CharPtr(chars); }
+
+		public CharPtr()
+		{
+			this.chars = null;
+			this.index = 0;
+		}
+
+		public CharPtr(string str)
+		{
+			this.chars = (str + '\0').ToCharArray();
+			this.index = 0;
+		}
+
+		public CharPtr(CharPtr ptr)
+		{
+			this.chars = ptr.chars;
+			this.index = ptr.index;
+		}
+
+		public CharPtr(CharPtr ptr, int index)
+		{
+			this.chars = ptr.chars;
+			this.index = index;
+		}
+
+		public CharPtr(char[] chars)
+		{
+			this.chars = chars;
+			this.index = 0;
+		}
+
+		public CharPtr(char[] chars, int index)
+		{
+			this.chars = chars;
+			this.index = index;
+		}
+
+		public CharPtr(IntPtr ptr)
+		{
+			this.chars = new char[0];
+			this.index = 0;
+		}
+
+		public static CharPtr operator +(CharPtr ptr, int offset) {return new CharPtr(ptr.chars, ptr.index+offset);}
+		public static CharPtr operator -(CharPtr ptr, int offset) {return new CharPtr(ptr.chars, ptr.index-offset);}
+		public static CharPtr operator +(CharPtr ptr, uint offset) { return new CharPtr(ptr.chars, ptr.index + (int)offset); }
+		public static CharPtr operator -(CharPtr ptr, uint offset) { return new CharPtr(ptr.chars, ptr.index - (int)offset); }
+
+		public void inc() { this.index++; }
+		public void dec() { this.index--; }
+		public CharPtr next() { return new CharPtr(this.chars, this.index + 1); }
+		public CharPtr prev() { return new CharPtr(this.chars, this.index - 1); }
+		public CharPtr add(int ofs) { return new CharPtr(this.chars, this.index + ofs); }
+		public CharPtr sub(int ofs) { return new CharPtr(this.chars, this.index - ofs); }
+		
+		public static bool operator ==(CharPtr ptr, char ch) { return ptr[0] == ch; }
+		public static bool operator ==(char ch, CharPtr ptr) { return ptr[0] == ch; }
+		public static bool operator !=(CharPtr ptr, char ch) { return ptr[0] != ch; }
+		public static bool operator !=(char ch, CharPtr ptr) { return ptr[0] != ch; }
+
+		public static CharPtr operator +(CharPtr ptr1, CharPtr ptr2)
+		{
+			string result = "";
+			for (int i = 0; ptr1[i] != '\0'; i++)
+				result += ptr1[i];
+			for (int i = 0; ptr2[i] != '\0'; i++)
+				result += ptr2[i];
+			return new CharPtr(result);
+		}
+		public static int operator -(CharPtr ptr1, CharPtr ptr2) {
+			Debug.Assert(ptr1.chars == ptr2.chars); return ptr1.index - ptr2.index; }
+		public static bool operator <(CharPtr ptr1, CharPtr ptr2) {
+			Debug.Assert(ptr1.chars == ptr2.chars); return ptr1.index < ptr2.index; }
+		public static bool operator <=(CharPtr ptr1, CharPtr ptr2) {
+			Debug.Assert(ptr1.chars == ptr2.chars); return ptr1.index <= ptr2.index; }
+		public static bool operator >(CharPtr ptr1, CharPtr ptr2) {
+			Debug.Assert(ptr1.chars == ptr2.chars); return ptr1.index > ptr2.index; }
+		public static bool operator >=(CharPtr ptr1, CharPtr ptr2) {
+			Debug.Assert(ptr1.chars == ptr2.chars); return ptr1.index >= ptr2.index; }
+		public static bool operator ==(CharPtr ptr1, CharPtr ptr2) {
+			object o1 = ptr1 as CharPtr;
+			object o2 = ptr2 as CharPtr;
+			if ((o1 == null) && (o2 == null)) return true;
+			if (o1 == null) return false;
+			if (o2 == null) return false;
+			return (ptr1.chars == ptr2.chars) && (ptr1.index == ptr2.index); }
+		public static bool operator !=(CharPtr ptr1, CharPtr ptr2) {return !(ptr1 == ptr2); }
+
+		public override bool Equals(object o)
+		{
+			return this == (o as CharPtr);
+		}
+
+		public override int GetHashCode()
+		{
+			return 0;
+		}
+		public override string ToString()
+		{
+			string result = "";
+			for (int i = index; (i<chars.Length) && (chars[i] != '\0'); i++)
+				result += chars[i];
+			return result;
+		}
 	}
 	
 	public class TKey_nk
@@ -274,5 +407,10 @@ namespace KopiLua
 	{
 		public TKey i_key;
 		public TValue i_val;	
+	}
+	
+	public class lua_Reader
+	{
+		
 	}
 }
--- lua-5.3.4/kopilua/lzio.h.cs
+++ lua-5.3.4/kopilua/lzio.h.cs
@@ -5,62 +5,70 @@
 */
 
 
-#ifndef lzio_h
-#define lzio_h
+namespace KopiLua
+{
+	public partial class Lua
+	{
 
-#include "lua.h"
+		
+		public const int EOZ = -1;			/* end of stream */
+		
+		public class ZIO : Zio {}
+		
+		public static int zgetc(ZIO z) { if (z.n-- > 0) {  int ch = cast_uchar(z.p[0]); z.p.inc(); return ch; } else {return luaZ_fill(z);} }
+		
+		
+		public class Mbuffer {
+			public CharPtr buffer = new CharPtr();
+		  	public uint n;
+		  	public uint buffsize;
+		}
+		
+		public static void luaZ_initbuffer(lua_State L, Mbuffer buff) { buff.buffer = null; buff.buffsize = 0; }
+		
+		public static CharPtr luaZ_buffer(Mbuffer buff)	{ return buff.buffer; }
+		public static uint luaZ_sizebuffer(Mbuffer buff) { return buff.buffsize; }
+		public static uint luaZ_bufflen(Mbuffer buff) { return buff.n; }
+		
+		public static void luaZ_buffremove(Mbuffer buff, int i)	{ buff.n -= (uint)i; } //FIXME:
+		public static void luaZ_resetbuffer(Mbuffer buff) { buff.n = 0; }
+		
+		//FIXME:???
+		public static void luaZ_resizebuffer(lua_State L, Mbuffer buff, int size) {
+			//FIXME:original
+			//buff.buffer = luaM_reallocvchar(L, buff.buffer,
+			//buff.buffsize, size);
+			
+			//-------------------
+			//FIXME:after
+			if (buff.buffer == null)
+				buff.buffer = new CharPtr();
+			luaM_reallocvector(L, ref buff.buffer.chars, (int)buff.buffsize, size);
+			//-------------------
+			
+			buff.buffsize = (uint)size; }
+		
+		public static void luaZ_freebuffer(lua_State L, Mbuffer buff) { luaZ_resizebuffer(L, buff, 0); }
+		
+		
+		//LUAI_FUNC void luaZ_init (lua_State *L, ZIO *z, lua_Reader reader,
+		//                                        void *data);
+		//LUAI_FUNC size_t luaZ_read (ZIO* z, void *b, size_t n);	/* read next n bytes */
+		
+		
+		
+		/* --------- Private Part ------------------ */
+		
+		public class Zio {
+		  public uint n;			/* bytes still unread */
+		  public CharPtr p;		/* current position in buffer */
+		  public lua_Reader reader;		/* reader function */
+		  public object data;			/* additional data */
+		  public lua_State L;			/* Lua state (for reader) */
+		}
+		
+		
+		//LUAI_FUNC int luaZ_fill (ZIO *z);
 
-#include "lmem.h"
-
-
-#define EOZ	(-1)			/* end of stream */
-
-typedef struct Zio ZIO;
-
-#define zgetc(z)  (((z)->n--)>0 ?  cast_uchar(*(z)->p++) : luaZ_fill(z))
-
-
-typedef struct Mbuffer {
-  char *buffer;
-  size_t n;
-  size_t buffsize;
-} Mbuffer;
-
-#define luaZ_initbuffer(L, buff) ((buff)->buffer = NULL, (buff)->buffsize = 0)
-
-#define luaZ_buffer(buff)	((buff)->buffer)
-#define luaZ_sizebuffer(buff)	((buff)->buffsize)
-#define luaZ_bufflen(buff)	((buff)->n)
-
-#define luaZ_buffremove(buff,i)	((buff)->n -= (i))
-#define luaZ_resetbuffer(buff) ((buff)->n = 0)
-
-
-#define luaZ_resizebuffer(L, buff, size) \
-	((buff)->buffer = luaM_reallocvchar(L, (buff)->buffer, \
-				(buff)->buffsize, size), \
-	(buff)->buffsize = size)
-
-#define luaZ_freebuffer(L, buff)	luaZ_resizebuffer(L, buff, 0)
-
-
-LUAI_FUNC void luaZ_init (lua_State *L, ZIO *z, lua_Reader reader,
-                                        void *data);
-LUAI_FUNC size_t luaZ_read (ZIO* z, void *b, size_t n);	/* read next n bytes */
-
-
-
-/* --------- Private Part ------------------ */
-
-struct Zio {
-  size_t n;			/* bytes still unread */
-  const char *p;		/* current position in buffer */
-  lua_Reader reader;		/* reader function */
-  void *data;			/* additional data */
-  lua_State *L;			/* Lua state (for reader) */
-};
-
-
-LUAI_FUNC int luaZ_fill (ZIO *z);
-
-#endif
+	}
+}
